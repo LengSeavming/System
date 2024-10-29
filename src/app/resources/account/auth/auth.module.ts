a177712f44a68e5a24011ceec0dc97bb7f2bfa64
CHAN Suvannet <chansuvannet999@gmail.com>
2024-10-30 00:10:35 +0700

    deletd sms

--- src/app/resources/account/auth/auth.module.ts
+++ src/app/resources/account/auth/auth.module.ts
@@ -3,13 +3,12 @@ import { Module } from '@nestjs/common';
 
 // ===========================================================================>> Costom Library
 import { EmailService } from '@app/services/email.service';
-import { TwilioService } from '@app/services/twilio.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService, EmailService, TwilioService]
+    providers: [AuthService, EmailService]
 })
 
 export class AuthModule { }
