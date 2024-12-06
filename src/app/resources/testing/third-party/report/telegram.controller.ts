bf4dd01dc5798780887007b718d830da6ede5d8e
Khouch Koeun <khouch.koeun@gmail.com>
2024-12-06 18:17:35 +0700

    report

---
+++ src/app/resources/testing/third-party/report/telegram.controller.ts
@@ -0,0 +1,23 @@
+// =========================================================================>> Core Library
+import { Body, Controller, Get, Query } from '@nestjs/common';
+
+// =========================================================================>> Custom Library
+import { TelegramService } from './telegram.service';
+
+// ======================================= >> Code Starts Here << ========================== //
+@Controller()
+export class TelegramController {
+
+    constructor(private readonly _service: TelegramService) { };
+
+    // ====================================================>> Sum 1
+    @Get('me')
+    async getMe(): Promise<{ result: number  }> {
+
+        return await this._service.getMe();
+
+    }
+
+
+    
+}
